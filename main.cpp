d7d7e695951dc38f80eb9a48ab4d5b7429597a52
Luis Gomes <gomes.luis479@gmail.com>
2019-03-25 12:51:58 +0530

    added comments

--- main.cpp
+++ main.cpp
@@ -1,28 +1,34 @@
+// Core openCV and image read/write headers.
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+
 #include <iostream>
 #include <cmath>
-
 
 using namespace cv;
 using namespace std;
 
-
+// Data type to store elements of the deviation
+// matrix for filtering.
 struct deviationElem {
     int x, y;
 };
 
+// Used as a data type in getMeanAndVariance() function.
 struct imageProperties{
     double mean, variance;
 };
 
-
+// Function declarations.
 Mat readImage(int argc, char** argv);
 Mat convertToGray(Mat& RGBimage);
-void displayImage(Mat& image, String windowName);
 Mat filerGrayImage(Mat& grayImage);
+void displayImage(Mat& image, String windowName);
+
+// Helper function to find parameters.
 imageProperties getMeanAndVariance(Mat& image);
 
+// Functions to calculate filtering quality parameters.
 double SSI(Mat& original, Mat& filtered);
 double MSE(Mat& original, Mat& filtered);
 double PSNR(Mat& original, Mat& filtered);
@@ -30,35 +36,7 @@ double ESIH(Mat& original, Mat& filtered);
 double ESIV(Mat& original, Mat& filtered);
 
 
-// test functions;
-void printGrayValues(Mat& grayImage, String imageName);
-Mat generateTestImage();
-
-
 int main(int argc, char** argv) {
-
-    // ------------------ test -------------
-
-    // Mat grayImage = generateTestImage();
-    // Mat filteredImage = filerGrayImage(grayImage);
-
-    // printGrayValues(grayImage, "input");
-    // printGrayValues(filteredImage, "filtered");
-
-    // struct imageProperties g = getMeanAndVariance(grayImage);
-    // cout << endl << g.mean << "  " << g.variance;
-
-    // double a = PSNR(grayImage, filteredImage);
-    // cout<<endl<<"PSNR: "<<a<<endl;
-
-    // double b = ESIH(grayImage, filteredImage);
-    // cout<<endl<<"ESIH: "<<b<<endl;
-
-    // double c = ESIV(grayImage, filteredImage);
-    // cout<<endl<<"ESIV: "<<c<<endl;
-
-    // -------------------------------------
-
 
     Mat inputImage = readImage(argc, argv);
     Mat grayImage = convertToGray(inputImage);
@@ -88,9 +66,10 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+
 Mat readImage(int argc, char** argv) {
 
-    // check if path to image is passed through command line
+    // Check if path to image is passed through command line.
     if( argc != 2) {
         cout << "Image not passed as argument" << endl;
         exit(1);
@@ -98,7 +77,7 @@ Mat readImage(int argc, char** argv) {
 
     Mat inputImage = imread(argv[1], CV_LOAD_IMAGE_COLOR);
 
-    // Check for invalid input
+    // Check for invalid file input.
     if(! inputImage.data ) {                             
         cout << "Could not open or find the image" << endl ;
         exit(1);
@@ -106,6 +85,7 @@ Mat readImage(int argc, char** argv) {
 
     return inputImage;
 }
+
 
 Mat convertToGray(Mat& RGBimage) {
     
@@ -116,22 +96,17 @@ Mat convertToGray(Mat& RGBimage) {
 
     double r_double, g_double, b_double;
     unsigned char gray;
-    unsigned char r, g, b;
-
+ 
     for(int i = 0; i < RGBimage.rows; i++) {
         for(int j = 0; j < RGBimage.cols; j++) {
 
-            b = rgbPointer[RGBimage.step*i + 3*j ] ;
-            g = rgbPointer[RGBimage.step*i + 3*j + 1];
-            r = rgbPointer[RGBimage.step*i + 3*j + 2];
+            b_double = rgbPointer[RGBimage.step*i + 3*j ] ;
+            g_double = rgbPointer[RGBimage.step*i + 3*j + 1];
+            r_double = rgbPointer[RGBimage.step*i + 3*j + 2];
 
-            r_double = r;
-            g_double = g;
-            b_double = b;
+            gray = round( (r_double + g_double + b_double) / 3 );
 
-            gray = (r_double + g_double + b_double) / 3; 
-
-            grayPointer[grayImage.step*i + j] = round(gray);
+            grayPointer[grayImage.step*i + j] = gray;
 
         }
     }
@@ -139,10 +114,12 @@ Mat convertToGray(Mat& RGBimage) {
     return grayImage;
 }
 
+
 void displayImage(Mat& image, String windowName) {
     namedWindow(windowName, WINDOW_AUTOSIZE);
     imshow(windowName, image);
 }
+
 
 Mat filerGrayImage(Mat& grayImage) {
     int n; // filter size
@@ -150,8 +127,7 @@ Mat filerGrayImage(Mat& grayImage) {
     double filter[11][11];
     double filterProduct = 0;
 
-    // -------------  Read filter size and elements  -------------
-
+    // Read filter size and elements of the filter.
     cout << endl << "Enter the filter size: (must be an odd number and <= 11) ";
     cin >> n;
 
@@ -162,12 +138,9 @@ Mat filerGrayImage(Mat& grayImage) {
         }
     }
 
-    // -----------------------------------------------------------
-
-
-
-
-    // -------------  Build deviation matrix  -------------
+    // Build deviation matrix.
+    // This stores the (x, y) deviation of each filter 
+    // element from the center element in terms of (x, y).
 
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < n; j++) {
@@ -175,18 +148,26 @@ Mat filerGrayImage(Mat& grayImage) {
             deviationMatrix[i][j].y = i - n/2;
         }
     }
+
+    // e.g in a 0 indexed 3x3 filter
+    // deviationMatrix[1][1] = (x, y) = (0, 0)
+    // deviationMatrix[0][0] = (x, y) = (-1, -1)
+    // deviationMatrix[2][2] = (x, y) = (1, 1)
+    // deviationMatrix[0][1] = (x, y) = (0, -1)
+
+    // The (x, y) values in the deviation matrix are used to
+    // get the shift in index for filtering.
     
-    // ---------------------------------------------------
 
-
-
-    // initialize filtered image container and required pointers
+    // Initialize filtered image container and required pointers.
     Mat filteredImage(grayImage.rows, grayImage.cols, CV_8U, Scalar(0));
     unsigned char* grayPointer = (unsigned char*)grayImage.data;
     unsigned char* filteredPointer = (unsigned char*)filteredImage.data;
 
     int x_shift, y_shift, x_index, y_index;
     unsigned char temp;
+
+    // Perform filtering.
     for(int i = 0; i < grayImage.rows; i++) {
         for(int j = 0; j < grayImage.cols; j++) {
 
@@ -200,7 +181,7 @@ Mat filerGrayImage(Mat& grayImage) {
                     x_index = i + y_shift;
                     y_index = j + x_shift;
 
-
+                    // Check if the shifted index is out of bounds for the given image.
                     if( (x_index<0) || (x_index>=grayImage.rows) || (y_index<0) || (y_index>=grayImage.cols) ){
                         continue;
                     }
@@ -226,6 +207,7 @@ Mat filerGrayImage(Mat& grayImage) {
     return filteredImage;
 }
 
+
 imageProperties getMeanAndVariance(Mat& image) {
     struct imageProperties returnValue;
 
@@ -235,6 +217,9 @@ imageProperties getMeanAndVariance(Mat& image) {
     double sum2 = 0;
     double grayValue;
 
+    // Mean = E(x)
+    // Variance = E(x^2) - Mean^2
+    
     for(int i = 0; i < image.rows; i++) {
         for(int j = 0; j < image.cols; j++) {
             grayValue =  imagePointer[image.step*i + j ]  ;
@@ -253,6 +238,7 @@ imageProperties getMeanAndVariance(Mat& image) {
 
 }
 
+
 double SSI(Mat& original, Mat& filtered) {
     struct imageProperties originalProps = getMeanAndVariance(original);
     struct imageProperties filteredProps = getMeanAndVariance(filtered);
@@ -265,8 +251,8 @@ double SSI(Mat& original, Mat& filtered) {
     double returnValue = ( sqrt(filteredVariance) * originalMean ) / ( filteredlMean * sqrt(originalVariance) );
 
     return returnValue; 
-
 }
+
 
 double MSE(Mat& original, Mat& filtered) {
     double sum = 0;
@@ -275,8 +261,10 @@ double MSE(Mat& original, Mat& filtered) {
     unsigned char* filteredPointer = (unsigned char*)filtered.data;
 
     double grayValue1, grayValue2;
+
     for(int i = 0; i < original.rows; i++) {
         for(int j = 0; j < original.cols; j++) {
+
             grayValue1 =  originalPointer[original.step*i + j ]  ;
             grayValue2 =  filteredPointer[filtered.step*i + j ]  ;
 
@@ -289,46 +277,12 @@ double MSE(Mat& original, Mat& filtered) {
     return returnValue;
 }
 
+
 double PSNR(Mat& original, Mat& filtered) {
     double returnValue = 10 * log10( (255*255) / MSE(original, filtered));
     return returnValue;
 }
 
-void printGrayValues(Mat& grayImage, String imageName) {
-
-    unsigned char* grayPointer = (unsigned char*)grayImage.data;
-
-    unsigned char grayLevel;
-    cout << endl << "------------   " << imageName << "   ------------" << endl;
-    for(int i = 0; i < grayImage.rows; i++) {
-        for(int j = 0; j < grayImage.cols; j++) {
-            grayLevel = grayPointer[grayImage.step*i + j ] ;
-            cout<<"  "<<int(grayLevel);
-        }
-        cout<<endl;
-    }
-}
-
-Mat generateTestImage() {
-    int rows = 3;
-    int cols = 3;
-    unsigned char test[3][3] = {
-        {10, 10, 10},
-        {100, 100, 100},
-        {10, 10, 10}
-    };
-
-    Mat grayImage(rows, cols, CV_8U, Scalar(0));
-    unsigned char* grayPointer = (unsigned char*)grayImage.data;
-
-    for(int i = 0; i < rows; i++) {
-        for(int j = 0; j < cols; j++) {
-            grayPointer[grayImage.step*i + j] = test[i][j];
-        }
-    }
-
-    return grayImage;
-}
 
 double ESIH(Mat& original, Mat& filtered) {
     double sum1 = 0;
@@ -338,6 +292,7 @@ double ESIH(Mat& original, Mat& filtered) {
     unsigned char* filteredPointer = (unsigned char*)filtered.data;
 
     double grayValue1, grayValue2, grayValue3, grayValue4;
+
     for(int i = 0; i < original.rows; i++) {
         for(int j = 0; j < original.cols - 1; j++) {
 
@@ -347,7 +302,7 @@ double ESIH(Mat& original, Mat& filtered) {
 
             sum1 += abs(grayValue1 - grayValue2);
 
-            //denominator
+            // denominator
             grayValue3 = originalPointer[original.step*i + (j+1) ];
             grayValue4 = originalPointer[original.step*i + j ];
 
@@ -361,6 +316,7 @@ double ESIH(Mat& original, Mat& filtered) {
     return returnValue;
 }
 
+
 double ESIV(Mat& original, Mat& filtered) {
     double sum1 = 0;
     double sum2 = 0;
@@ -369,6 +325,7 @@ double ESIV(Mat& original, Mat& filtered) {
     unsigned char* filteredPointer = (unsigned char*)filtered.data;
 
     double grayValue1, grayValue2, grayValue3, grayValue4;
+
     for(int j = 0; j < original.cols - 1; j++) {
         for(int i = 0; i < original.rows; i++) {
 
@@ -378,7 +335,7 @@ double ESIV(Mat& original, Mat& filtered) {
 
             sum1 += abs(grayValue1 - grayValue2);
 
-            //denominator
+            // denominator
             grayValue3 = originalPointer[original.step*i + j ];
             grayValue4 = originalPointer[original.step*(i+1) + j ];
 
